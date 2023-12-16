374a3f4b192decc1ab14f93d61d39af56c3c8f9a
ztygy <92307536+ztygy@users.noreply.github.com>
2023-12-16 23:57:10 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -10,7 +10,7 @@ const CONFIG = {
   ],
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: 'unraid',
+  HEO_HERO_TITLE_1: '数字之舞',
   HEO_HERO_TITLE_2: '教程&随记',
   HEO_HERO_TITLE_3: 'TANGLY1024.COM',
   HEO_HERO_TITLE_4: '新版上线',
