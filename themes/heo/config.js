afc5e3e5915ae98f1c476b576049e972da02b042
ztygy <92307536+ztygy@users.noreply.github.com>
2023-12-17 22:05:59 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,7 +5,7 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://bing.netcarshow.top/' },
+    { title: '欢迎来到我的博客', url: 'https://bing.netcarshow.top/?theme=hexo' },
     { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
   ],
 
