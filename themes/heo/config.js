3e8446eb1cb37f6f827655cd60a60fffdf1d46a1
ztygy <92307536+ztygy@users.noreply.github.com>
2023-12-25 09:14:15 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,9 +18,9 @@ const CONFIG = {
   HEO_HERO_TITLE_LINK: 'https://bing.netcarshow.top',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: 'unRAID', url: '/tag/unRAID' },
-  HEO_HERO_CATEGORY_2: { title: '3D打印', url: '/tag/3D打印' },
-  HEO_HERO_CATEGORY_3: { title: '其他教程', url: '/tag/其他教程' },
+  HEO_HERO_CATEGORY_1: { title: 'unRAID', url: '/category/unRAID' },
+  HEO_HERO_CATEGORY_2: { title: '3D打印', url: '/category/3D打印' },
+  HEO_HERO_CATEGORY_3: { title: '其他教程', url: '/category/其他教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
